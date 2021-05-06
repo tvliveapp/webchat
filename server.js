2c244455f2a4289968bbbb5411748067c969afbe
tvliveapp <61073350+tvliveapp@users.noreply.github.com>
2021-05-06 00:35:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 'use strict';
-
+/*
 const express = require('express');
 const { Server } = require('ws');
-
+*/
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 /*
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-*/
 
+/*
 
 const wss = new Server({ server });
 
@@ -31,12 +31,11 @@ function onMsg(msg){
 		
     }); 
 }
-/*
 console.log(wss.clients)
 	wss.clients.forEach(function each(client) {
         client.send(data);
     });
-*/
+
 wss.on('connection', function connection(ws) {
   console.log("new...");
   ws.on('message', function incoming(data) {
@@ -75,3 +74,28 @@ http.createServer(function(request, response) {
 }).listen(PORT);
 
 
+*/
+
+var http = require('http');
+var sys  = require('sys');
+
+http.createServer(function(request, response) {
+  sys.log(request.connection.remoteAddress + ": " + request.method + " " + request.url);
+  var proxy = http.createClient(80, request.headers['host'])
+  var proxy_request = proxy.request(request.method, request.url, request.headers);
+  proxy_request.addListener('response', function (proxy_response) {
+    proxy_response.addListener('data', function(chunk) {
+      response.write(chunk, 'binary');
+    });
+    proxy_response.addListener('end', function() {
+      response.end();
+    });
+    response.writeHead(proxy_response.statusCode, proxy_response.headers);
+  });
+  request.addListener('data', function(chunk) {
+    proxy_request.write(chunk, 'binary');
+  });
+  request.addListener('end', function() {
+    proxy_request.end();
+  });
+}).listen(PORT);
